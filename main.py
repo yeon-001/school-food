d24afe44393ccaa92ae08576d1931c89d3379029
yeon-001 <si0103@goedu.kr>
2026-09-23 12:40:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-```python
 import streamlit as st
 import requests
 import pandas as pd
@@ -492,4 +491,3 @@ if schools:
                     f"일부 날짜는 API 응답 문제로 제외되었습니다: "
                     f"{error_count}일"
                 )
-```
